31da8a594ff7d9e24da5f7937afebad66f88ff17
Akash Singh <asakashrohit8@gmail.com>
2025-01-29 12:38:49 +0530

    completing javascript action

--- .github/actions/js-dependency-updates/index.js
+++ .github/actions/js-dependency-updates/index.js
@@ -1,5 +1,6 @@
 const core = require("@actions/core");
 const exec = require("@actions/exec");
+const github = require("@actions/github");
 
 (() => {
   async function run() {
@@ -29,9 +30,38 @@ const exec = require("@actions/exec");
       }
     );
 
-    if (gitStatus.stdout.length > 0)
-      core.info(`[js-dependency-update]: There are updates available`);
-    else core.info(`[js-dependency-update]: There are no updates available`);
+    if (gitStatus.stdout.length > 0) {
+      try {
+        core.info(`[js-dependency-update]: There are updates available`);
+        await exec.exec('git config --global user.name "gh-automation"');
+        await exec.exec(
+          'git config --global user.email "gh-automation@email.com"'
+        );
+        await exec.exec(`git checkout -b ${targetBranch}`, [], {
+          cwd: workingDir,
+        });
+        await exec.exec("git add package.json package-lock.json");
+        await exec.exec(
+          'git commit -m "automation: updating the dependency package"'
+        );
+        await exec.exec(`git push -u origin ${targetBranch} --force`);
+        const octokit = github.getOctokit(ghToken);
+        await octokit.rest.pulls.create({
+          owner: github.context.repo.owner,
+          repo: github.context.repo.repo,
+          title: "Update NPM dependency",
+          body: "This PR request updates the NPM libraries",
+          base: baseBranch,
+          head: targetBranch,
+        });
+      } catch (error) {
+        core.error("Unable to create the PR");
+        core.setFailed(error.message);
+        core.error(error);
+      }
+    } else {
+      core.info(`[js-dependency-update]: There are no updates available`);
+    }
   }
 
   run();
